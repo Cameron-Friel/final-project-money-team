096197520860f6170c941129a42d7d0f2b8535b1
Cameron Friel <frielc@flip2.engr.oregonstate.edu>
2017-06-08 22:09:28 -0700

    changed server.js to allow for new css file named about.css

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ app.get('/suspects', function(req, res, next) //Renders page which shows suspect
   res.status(200);
 });
 
-app.get('/about', function() //Renders about page
+app.get('/about', function(req, res, next) //Renders about page
 {
-  res.render('about'); //{layout: 'about'} to be changed when we need a new css file!!!
+  res.render('about', layout: 'aboutus'); //apply a new layout to the about page
   res.status(200);
 });
 
