c4ec68e3805ea4dbefe0a6b89ba3057d991c5b68
Cameron Friel <frielc@flip1.engr.oregonstate.edu>
2017-06-10 13:11:07 -0700

    created functions to route to new pages when murderer is found or when 3 guesses are used

--- public/index.js
+++ public/index.js
@@ -1,5 +1,11 @@
 var suspects = document.getElementsByClassName('suspect-container');
 var suspectHolder = []; //array to hold suspect containers
+var counter = 0; //var to keep track of how many guesses user has used up (max = 3)
+
+suspects[murderIndex].addEventListener('click', function()
+{
+  location.href = "about"; //WE WANT THIS TO ROUTE TO WINNER PAGE SENDING MURDERER DATA TO PAGE!
+});
 
 for (var i = suspects.length - 1; i >= 0; i--)
 {
@@ -8,9 +14,16 @@ for (var i = suspects.length - 1; i >= 0; i--)
 
 function guessSuspect(i) //function to delete suspects as they are clicked
 {
-
   return function()
   {
+    console.log("Index: ", murderIndex);
+
+    if (counter == 2 && murderIndex != i)
+    {
+      location.href = "404Page";
+    }
+    counter++; //Increment for each guess that is wrong
+
     for (var j = 0; j < suspects.length; j++)
     {
       if (suspects[j] == suspectHolder[i]) //Compares
