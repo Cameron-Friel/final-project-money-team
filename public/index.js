f2e928a916e464bfd388df5821c70bd33c954dc4
Cameron Friel <frielc@flip3.engr.oregonstate.edu>
2017-06-09 21:43:32 -0700

    made server delete suspect containers for the main page

--- public/index.js
+++ public/index.js
@@ -1,10 +1,31 @@
 var suspects = document.getElementsByClassName('suspect-container');
-console.log(suspects[0]);
+var suspectHolder = []; //array to hold suspect containers
+
 for (var i = suspects.length - 1; i >= 0; i--)
 {
-  suspects[i].addEventListener('click', function()
-  {
-    console.log("hit");
-    suspects.remove(suspects.i); //WHY DOES THIS NOT REMOVE MY SELECTED SUSPECT!?!??
-  });
+  suspects[i].addEventListener('click', guessSuspect(i));
 }
+
+function guessSuspect(i) //function to delete suspects as they are clicked
+{
+
+  return function()
+  {
+    for (var j = 0; j < suspects.length; j++)
+    {
+      if (suspects[j] == suspectHolder[i]) //Compares
+      {
+        suspects[j].remove();
+        break;
+      }
+    }
+  };
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+
+  var temp = document.getElementsByClassName('suspect-container'); //hold suspect containers
+  for (var i = 0; i < temp.length; i++) {
+    suspectHolder.push(temp[i]); //push them into array to be fetched while game goes on
+  }
+});
