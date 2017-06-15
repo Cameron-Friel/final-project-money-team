b7a2d5c1b78525224ae55ed112b8e4b72186ba53
Vincent Ta <Vincent Ta>
2017-06-14 17:21:43 -0700

    updateSuspectCreateButton

--- public/suspects.js
+++ public/suspects.js
@@ -3,9 +3,22 @@
 var getModal = document.getElementById('add-suspect-button');
 var backdrop = document.getElementById('modal-backdrop');
 var twitModal = document.getElementById('create-twit-modal');
+var closeButton = document.querySelector('.modal-close-button');
+var cancelButton = document.querySelector('.modal-cancel-button');
 
 getModal.addEventListener('click', function()
 {
   backdrop.classList.remove('hidden');
   twitModal.classList.remove('hidden');
+});
+
+
+closeButton.addEventListener('click', function () {
+  backdrop.classList.add('hidden');
+  twitModal.classList.add('hidden');
+});
+
+cancelButton.addEventListener('click', function () {
+  backdrop.classList.add('hidden');
+  twitModal.classList.add('hidden');
 });
